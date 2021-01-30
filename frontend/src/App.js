44555399b5cadefc91de747e214e0cd0386b7fe8
Sulav Acharya <sa9ac@virginia.edu>
2021-01-29 22:40:03 -0500

    Add Earth pic to the splash page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
-import BannerPic from './assets/allotropy.png'
+import BannerPic from './assets/allotropy.png';
+import world from './assets/worldwide.png';
 import './App.css';
 import React from 'react';
 import { Header } from 'semantic-ui-react'
@@ -15,10 +16,10 @@ function App() {
         <img src={BannerPic} style = {{width: '20%', marginTop: '-5%', marginLeft: '85%'}} />
          <h1 style={{color:'#ffffff', marginRight: '65%', marginTop: '-9%', fontSize: '3.5em' }}>Calculate Carbon</h1>
          <h1 style={{color:'#ffffff', marginRight: '76%', fontSize: '3.5em' }}>Emission</h1>
+         <img src={world} style={{ width: '5%', marginRight: '55%', marginTop: '-11%' }} />
         </ParallaxLayer>
         <ParallaxLayer offset={1} speed={0.1} factor={1.5} style={{ backgroundColor: '#1545C0' }} />
         <ParallaxLayer offset={2} speed={0.1} factor={3} style={{ backgroundColor: '#1545C0' }} />
-
         <ParallaxLayer offset={1} speed={0.5} style={{}}>
         <img src={url('earth')} style={{ width: '60%' }} />
         </ParallaxLayer>
