e7907a5eb865ca44447c9afee39c542d069937fb
PerryProjects <nperry@terpmail.umd.edu>
2021-01-30 20:06:07 -0500

    heatmap

---
+++ frontend/src/components/DataViz.js
@@ -0,0 +1,68 @@
+import React from "react";
+import DeckGL from "@deck.gl/react";
+import { LineLayer } from "@deck.gl/layers";
+import { StaticMap } from "react-map-gl";
+import {HeatmapLayer} from '@deck.gl/aggregation-layers';
+
+const MAPBOX_ACCESS_TOKEN =
+  "pk.eyJ1IjoibHVrZXBlcnJ5NDciLCJhIjoiY2tra2E5bXdkMThiNjMxb2R3ZXd2MjVzeiJ9.ANk2BfLUiL-fBvB7zbm5hA";
+
+// Viewport settings
+const INITIAL_VIEW_STATE = {
+  longitude: -98.5795,
+  latitude: 39.8283,
+  zoom: 1,
+  pitch: 0,
+  bearing: 0
+};
+
+// Data to be used by the LineLayer
+// const data = [
+//   {
+//     sourcePosition: [-122.41669, 37.7853],
+//     targetPosition: [-122.41669, 37.781]
+//   }
+// ];
+// new LineLayer({ id: "line-layer", data })
+
+const data = [
+  {
+      COORDINATES: [-77.0369, 38.9072], WEIGHT: .05
+  },
+  {
+      COORDINATES: [-122.42177834, 37.78346622], WEIGHT: .5
+  },
+
+];
+
+
+class DataViz extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    };
+    ComponentDidMount() {
+      //fetch
+      // for i in
+      //  this.state.data[0]['key'] = 'value'
+    };
+    render () {
+        const layers = [new HeatmapLayer({
+            id: 'heatmapLayer',
+            data,
+            getPosition: d => d.COORDINATES,
+            getWeight: d => d.WEIGHT,    
+          })];
+        return(<DeckGL
+            initialViewState={INITIAL_VIEW_STATE}
+            controller={true}
+            layers={layers}
+          >
+            <StaticMap mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN} />
+          </DeckGL>);
+    }
+
+
+}
+
+export default DataViz;
