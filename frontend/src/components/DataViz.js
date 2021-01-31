e0db8b150bd05796f9fc3184adcefad5a20dc03c
PerryProjects <nperry@terpmail.umd.edu>
2021-01-30 21:43:03 -0500

    connect to backend

--- frontend/src/components/DataViz.js
+++ frontend/src/components/DataViz.js
@@ -3,6 +3,7 @@ import DeckGL from "@deck.gl/react";
 import { LineLayer } from "@deck.gl/layers";
 import { StaticMap } from "react-map-gl";
 import {HeatmapLayer} from '@deck.gl/aggregation-layers';
+import axios from "react";
 
 const MAPBOX_ACCESS_TOKEN =
   "pk.eyJ1IjoibHVrZXBlcnJ5NDciLCJhIjoiY2tra2E5bXdkMThiNjMxb2R3ZXd2MjVzeiJ9.ANk2BfLUiL-fBvB7zbm5hA";
@@ -26,12 +27,12 @@ const INITIAL_VIEW_STATE = {
 // new LineLayer({ id: "line-layer", data })
 
 const data = [
-  {
-      COORDINATES: [-77.0369, 38.9072], WEIGHT: .05
-  },
-  {
-      COORDINATES: [-122.42177834, 37.78346622], WEIGHT: .5
-  },
+  // {
+  //     COORDINATES: [-77.0369, 38.9072], WEIGHT: .05
+  // },
+  // {
+  //     COORDINATES: [-122.42177834, 37.78346622], WEIGHT: .5
+  // },
 
 ];
 
@@ -42,10 +43,17 @@ class DataViz extends React.Component {
         this.state = {};
     };
     componentDidMount() {
+
       fetch('http://localhost:8000/emissions/')
-        .then(data => console.log(data));
-      // for i in
-      //  this.state.data[0]['key'] = 'value'
+        .then(response => response.json())
+        .then(content => content.list_emission_entries.map((entry) => {
+          const obj = {};
+          console.log(entry.normalized_emissions);
+          obj['COORDINATES'] = [entry.longitude, entry.lattitude];
+          obj['WEIGHT'] = entry.normalized_emissions;
+          data.push(obj);
+        }));
+        console.log(data)
     };
     render () {
         const layers = [new HeatmapLayer({
